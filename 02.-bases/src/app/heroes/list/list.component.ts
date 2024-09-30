e78f50743a46a52c6bd2d023dfa803d6e0719072
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-09-30 11:42:57 -0500

    Update editorconfig and add hero and counter modules

---
+++ 02.-bases/src/app/heroes/list/list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+export class ListComponent {
+  public heroName: string[] = [
+    'Spiderman',
+    'Iroman',
+    'Hulk',
+    'She Hulk',
+    'Ryan',
+  ];
+  public deleteHero?: string;
+  removeLastHero(): void {
+    this.deleteHero = this.heroName.pop();
+  }
+}
