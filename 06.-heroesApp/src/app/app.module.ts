5ff90a0886d47966d02cb9590da9e3622b82e5f9
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-11-25 10:46:14 -0500

    Agregar interfaz de héroe y servicio para obtener héroes; actualizar rutas y componentes para mostrar héroes

--- 06.-heroesApp/src/app/app.module.ts
+++ 06.-heroesApp/src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { RouterModule } from '@angular/router';
-import { HeroesModule } from './heroes/heroes.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {  HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +15,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     AppRoutingModule,
     RouterModule,
     SharedModule,
+    HttpClientModule
     // HeroesModule,
   ],
   providers: [
--- 06.-heroesApp/src/app/heroes/pages/list-page/list-page.component.ts
+++ 06.-heroesApp/src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,7 +1,16 @@
-import {  Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
 })
-export class ListPageComponent { }
+export class ListPageComponent implements OnInit {
+  constructor(private heroService: HeroesService) {}
+
+  public heroes: Hero[] = [];
+  ngOnInit(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+}
