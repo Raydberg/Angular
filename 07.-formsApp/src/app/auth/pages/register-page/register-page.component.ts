83ea1a1dad5ad16bac262b4630815e725e3529c5
Raydberg <raydga_2005@hotmail.com>
2024-12-11 01:20:08 -0500

    Agregar módulo de autenticación en AuthApp; implementar controladores, DTOs, y esquemas de usuario, y configurar conexión a MongoDB.

--- 07.-formsApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07.-formsApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,7 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  canBeStrider,
+  emailPattern,
+  firstNameAndLastnamePattern,
+} from '../../../shared/validators/validators.functions';
 
 @Component({
   selector: 'auth-register-page',
   templateUrl: './register-page.component.html',
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  public myForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.myForm = fb.group({
+      name: [
+        '',
+        [Validators.required, Validators.pattern(firstNameAndLastnamePattern)],
+      ],
+      email: ['', [Validators.required, Validators.pattern(emailPattern)]],
+      username: ['', [Validators.required, canBeStrider]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required]],
+    });
+  }
+
+  isValidField(field: string) {
+    //* Validacion desde un servicio
+  }
+
+  onSubmit(): void {
+    this.myForm.markAllAsTouched();
+  }
+}
--- 07.-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 07.-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,7 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'reactive-switches-page',
-  templateUrl: './switches-page.component.html'
+  templateUrl: './switches-page.component.html',
 })
-export class SwitchesPageComponent { }
+export class SwitchesPageComponent implements OnInit {
+  public myForm: FormGroup;
+  public person = {
+    gender: 'F',
+    wantNotifications: false,
+  };
+  constructor(private fb: FormBuilder) {
+    this.myForm = fb.group({
+      gender: ['M', Validators.required],
+      wantNotifications: [true, Validators.required],
+      termsAndConditions: [false, Validators.requiredTrue],
+    });
+  }
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
+  }
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+    console.log(this.myForm.value);
+    this.person = newPerson;
+  }
+}
